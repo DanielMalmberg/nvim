8cc49986f0657ba53432c6045ac59d2f3f155b54
DanielMalmberg <daniel.malmberg@live.com>
2023-04-22 00:29:35 +0200

    Initialize in init.lua

--- init.lua
+++ init.lua
@@ -3,3 +3,10 @@ require('plugin_configs')
 require('settings')
 require('keymaps')
 require('features')
+
+
+-- Initialize
+-----------------------------------------------------------
+
+-- Backgroundcolor
+require('utils').change_backgroundcolor(require('settings.gui_settings').current_bg)
