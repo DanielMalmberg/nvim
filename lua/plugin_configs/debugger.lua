3b6553258b7cd40530bdb18f5c8ff46552870ed0
DanielMalmberg <daniel.malmberg@live.com>
2023-04-22 21:09:39 +0200

    Remove unnecessary comments

--- lua/plugin_configs/debugger.lua
+++ lua/plugin_configs/debugger.lua
@@ -6,10 +6,7 @@ local dapui = require 'dapui'
 require('mason-nvim-dap').setup {
       automatic_setup = true,
       handlers = {},
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
       },
     }
