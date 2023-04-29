c84619cb2b8568531b0399250d85369cddcdcf26
DanielMalmberg <daniel.malmberg@live.com>
2023-04-29 23:49:02 +0200

    Fix backgroundcolor in toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,12 +4,7 @@ require('toggleterm').setup({
   size = 20,
   open_mapping = [[<C-ö>]],
   hide_numbers = true,
-  -- shade_terminals = true,
-  -- shading_factor = 100,
-  direction = "horizontal",
-  highlights = {
-    Normal = {
-      guibg = "NONE"
-    }
-  }
+  shade_terminals = true,
+  shading_factor = 0.5,
+  direction = "horizontal"
 })
