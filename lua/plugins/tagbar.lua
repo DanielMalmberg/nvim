f8eb425f66cd6f262df44d995506733678538428
DanielMalmberg <daniel.malmberg@live.com>
2023-05-01 15:36:36 +0200

    Autoclose tagbar when jumping to tag

--- lua/plugins/tagbar.lua
+++ lua/plugins/tagbar.lua
@@ -5,3 +5,6 @@ vim.api.nvim_exec('let g:tagbar_compact = 2', false)
 
 -- Jumps cursor to the tagbar when it's opened
 vim.api.nvim_exec('let g:tagbar_autofocus = 1', false)
+
+-- Automatically closes the tagbar when jumping to a tag (by pressing enter)
+vim.api.nvim_exec('let g:tagbar_autoclose = 1', false)
