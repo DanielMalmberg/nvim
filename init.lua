336b2121dc120c97b2a84bad23ba4037e9e37916
Daniel Malmberg <daniel.malmberg@live.com>
2023-12-21 13:50:09 +0100

    Add info about plugins with locked versions

--- init.lua
+++ init.lua
@@ -1,10 +1,16 @@
 -- INFO: Dependencies
--- -----------------------------------------------------------
 -- neovim
 -- node.js
 -- ripgrep (for Telescope Live Grep)
 -- fd (for Telescope - https://github.com/sharkdp/fd) 
 -- Universal Ctags (for Tagbar - https://ctags.io/)
+-- -----------------------------------------------------------
+
+-- INFO: Locked plugin versions
+-- indent_blankline.nvim (commit "b7aa0aed55887edfaece23f7b46ab22232fc8741") 
+-- - Plugin version 3 not compatible, use version 2 
+-- toggle-lsp-diagnostics.nvim (commit "77399c5f8fc68541b222aeae278afd0988ab660c")
+--  - Annoying popup message added in newer vesions when toggling diagnostics on
 -- -----------------------------------------------------------
 
 --  Must happen before plugins are required (otherwise wrong leader will be used)
