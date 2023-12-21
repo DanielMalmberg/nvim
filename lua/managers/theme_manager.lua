9c16d6e953504f44e007291e6c8bff49596af393
Daniel Malmberg <daniel.malmberg@live.com>
2023-12-21 13:23:58 +0100

    Fix transparency toggle function

--- lua/managers/theme_manager.lua
+++ lua/managers/theme_manager.lua
@@ -2,36 +2,55 @@
 
 local theme = require('settings.themes')
 local colors = require('settings.colors')
-
-local function change_backgroundcolor(color)
-  for _, property in pairs(colors.backgrounds) do
-    vim.cmd(string.format('highlight %s guibg=%s', property, color))
-  end
-end
-
 local M = {}
-M.toggle_transparency = function()
-  if theme.selected_theme.transparency then
-    local default_bg = theme.selected_theme.bg
-    if theme.current_bg == "NONE" then
-      theme.current_bg = default_bg
-    else
-      theme.current_bg = "NONE"
-    end
-    change_backgroundcolor(theme.current_bg)
-  end
+M.background_colors = {}
+M.transparent = false
+M.initialized = false
+
+local function set_background_color(element, color)
+  vim.cmd(string.format('highlight %s guibg=%s', element, color))
 end
 
--- INITIALIZE
-----------------------------------
-vim.opt.background = 'dark'
-if theme.selected_theme.transparency then
-  change_backgroundcolor(theme.current_bg)
-end
-if theme.selected_theme.custom_colors ~= nil then
-  for key, color in pairs(theme.selected_theme.custom_colors) do
-    vim.cmd(string.format('hi %s %s', key, color))
+M.toggle_transparency = function()
+  for index, element in ipairs(colors.backgrounds) do
+    local highlight_exists, _ = pcall(vim.api.nvim_get_hl_by_name, element, true)
+    if highlight_exists then
+      -- Save hightlight color
+      if not M.background_colors[element] then 
+        local output = vim.api.nvim_command_output('hi normal')
+        local guibg = string.match(output, 'guibg=([^%s]+)')
+        if guibg ~= nil then
+          M.background_colors[element] = guibg
+        end
+      end
+      -- Toggle off transparency
+      if M.transparent and M.background_colors[element] then 
+        set_background_color(element, M.background_colors[element])
+      -- Toggle on transparency
+      elseif not M.transparent then 
+        set_background_color(element, "NONE")
+      end
+    end
   end
+  -- Reverse toggle
+  M.transparent = not M.transparent
+end
+
+
+-- Initialize settings
+----------------------------------
+if not M.initialized then
+  -- Set custom colors before (!) toggling transparency
+  vim.opt.background = 'dark'
+  if theme.selected_theme.custom_colors ~= nil then
+    for key, color in pairs(theme.selected_theme.custom_colors) do
+      vim.cmd(string.format('hi %s %s', key, color))
+    end
+  end
+  if theme.transparent_at_startup then
+    M.toggle_transparency()
+  end
+  M.initialized = true
 end
 
 return M
--- lua/settings/themes.lua
+++ lua/settings/themes.lua
@@ -1,25 +1,22 @@
 -- [[ GUI Settings ]
 
 local colors = require('settings.colors')
-
 local M = {}
 
 M.selected_theme = {
-    -- name = '', source = '', transparency = false
-    -- name = 'vscode', source = 'mofiqul/vscode.nvim', transparency = true, bg = '#1e1e1e', custom_colors = colors.vscode_custom_colors
-    -- name = 'onedark', source = 'navarasu/onedark.nvim', transparency = false, --bg = '#1e1e1e'
-    -- name = 'tokyonight', source = 'folke/tokyonight.nvim', transparency = false, --bg = '#242635'
-    -- name = 'everforest', source = 'sainnhe/everforest', transparency = false, --bg = '#282F34'
-    -- name = 'nightfly', source = 'bluz71/vim-nightfly-colors', transparency = false, --bg = '#0A1522'
-    -- name = 'rose-pine-moon', source = 'rose-pine/neovim', transparency = false, --bg = '#1E1823'
-    -- name = 'quantum' source = 'tyrannicaltoucan/vim-quantum', transparency = false, --bg = '#1e1e1e'
-    -- name = 'catppuccin-mocha', source = 'catppuccin/nvim', transparency = false, -- bg = '#212332'
-    -- name = 'falcon', source = 'fenetikm/falcon', transparency = false, -- bg = '#1e1e1e'
-    name = 'tundra', source = 'sam4llis/nvim-tundra', transparency = false
+    -- name = '', source = '', custom_colors = {} (optional)
+    -- name = 'vscode', source = 'mofiqul/vscode.nvim', custom_colors = colors.vscode_custom_colors
+    -- name = 'onedark', source = 'navarasu/onedark.nvim'
+    -- name = 'tokyonight', source = 'folke/tokyonight.nvim'
+    -- name = 'everforest', source = 'sainnhe/everforest'
+    -- name = 'nightfly', source = 'bluz71/vim-nightfly-colors'
+    -- name = 'rose-pine-moon', source = 'rose-pine/neovim'
+    -- name = 'quantum' source = 'tyrannicaltoucan/vim-quantum'
+    -- name = 'catppuccin-mocha', source = 'catppuccin/nvim'
+    -- name = 'falcon', source = 'fenetikm/falcon'
+    name = 'tundra', source = 'sam4llis/nvim-tundra'
 }
 
--- Startup background (when toggle transparency is active)
-M.current_bg = M.selected_theme.bg
--- M.current_bg = "NONE"
+M.transparent_at_startup = false
 
 return M
