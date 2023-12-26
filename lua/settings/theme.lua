91d3a1da4774a405ee80761ecbee61cda895d02f
Daniel Malmberg <daniel.malmberg@live.com>
2023-12-26 19:09:54 +0100

    Add and remove some themes

--- lua/settings/theme.lua
+++ lua/settings/theme.lua
@@ -3,14 +3,13 @@
 local colors = require('settings.colors')
 -- name = '', source = '', custom_colors = {} (optional table from settings.colors)
 return {
+    name = 'tundra', source = 'sam4llis/nvim-tundra'
     -- name = 'vscode', source = 'mofiqul/vscode.nvim'
     -- name = 'onedark', source = 'navarasu/onedark.nvim'
     -- name = 'tokyonight', source = 'folke/tokyonight.nvim'
-    -- name = 'everforest', source = 'sainnhe/everforest'
     -- name = 'nightfly', source = 'bluz71/vim-nightfly-colors'
     -- name = 'rose-pine-moon', source = 'rose-pine/neovim'
-    -- name = 'quantum' source = 'tyrannicaltoucan/vim-quantum'
     -- name = 'catppuccin-mocha', source = 'catppuccin/nvim'
+    -- name = 'dracula', source = 'Mofiqul/dracula.nvim'
     -- name = 'falcon', source = 'fenetikm/falcon'
-    name = 'tundra', source = 'sam4llis/nvim-tundra'
 }
